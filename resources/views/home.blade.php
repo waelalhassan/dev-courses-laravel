d6ad621d6cb6623035d81001ecfb7bc20bceeebb
wael alhassan <waelalhassan.fed@gmail.com>
2022-03-21 18:06:13 +0300

    m

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,262 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Home</title>
+    <link rel="stylesheet" href="{{asset("css/app.css")}}">
 </head>
 <body>
-    <h1>Hello</h1>
+    <nav>
+        <div class="container">
+            <div class="nav-wrapper d-flex d-justify-between d-align-center">
+                <div class="brand-name">
+                    <h1><a href="#">DEV COURSES</a></h1>
+                </div>
+                <ul class="d-flex">
+                    <li><a href="#">link 1</a></li>
+                    <li><a href="#">link 2</a></li>
+                    <li><a href="#">link 3</a></li>
+                    <li><a href="#">link 4</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <header>
+        <div class="container">
+            <div class="wrapper d-flex d-justify-between d-align-center">
+                <div>
+                    <h2>
+                        Welcome to <br>
+                        Dot Net Mastery
+                    </h2>
+                    <p>The ultimate learning recourse for .NET developers.</p>
+                </div>
+                <div>
+                    <img src="" alt="header image">
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section>
+        <div class="container">
+            <div class="wrapper d-flex d-justify-between d-align-center">
+                <div>
+                    <h2></h2>
+                    <p>Welcome to DotNetMastery, our mission is to provide the most comprehensive traning for .NET Developers.</p>
+                    <p>We want to make coding and software engineering accessible to everyone through courses that are simple to follow, but still give exposure to solve real world challenges. All the courses have real world exposure to make sure, not just to teach you technology but make you ready for real world challanges.</p>
+                    <p>Most important part about writing and teaching code is to eliminate the hardships of every student out there who is willing to learn but cannot find adequate resource which is easy to understand.</p>
+                    <p>Coding is a way to be creative and have fun!</p>
+                </div>
+                <div>
+                    <img src="" alt="section 1 image">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <h2>What Dot Net Mastery Courses Offers</h2>
+            <div class="wrapper d-flex d-wrap-row">
+                <div class="sec-2-item d-flex d-align-center d-justify-between">
+                    <div class="sec-2-icon">
+                        <img src="" alt="icon">
+                    </div>
+                    <div class="sec-2-info">
+                        <h3>Job focused Training</h3>
+                        <p>All courses cover’s Real-world Examples with the best techniques along with displaying the errors and how to resolve them.</p>
+                    </div>
+                </div>
+                <div class="sec-2-item d-flex d-align-center d-justify-between">
+                    <div class="sec-2-icon">
+                        <img src="" alt="icon">
+                    </div>
+                    <div class="sec-2-info">
+                        <h3>Job focused Training</h3>
+                        <p>All courses cover’s Real-world Examples with the best techniques along with displaying the errors and how to resolve them.</p>
+                    </div>
+                </div>
+                <div class="sec-2-item d-flex d-align-center d-justify-between">
+                    <div class="sec-2-icon">
+                        <img src="" alt="icon">
+                    </div>
+                    <div class="sec-2-info">
+                        <h3>Job focused Training</h3>
+                        <p>All courses cover’s Real-world Examples with the best techniques along with displaying the errors and how to resolve them.</p>
+                    </div>
+                </div>
+                <div class="sec-2-item d-flex d-align-center d-justify-between">
+                    <div class="sec-2-icon">
+                        <img src="" alt="icon">
+                    </div>
+                    <div class="sec-2-info">
+                        <h3>Job focused Training</h3>
+                        <p>All courses cover’s Real-world Examples with the best techniques along with displaying the errors and how to resolve them.</p>
+                    </div>
+                </div>
+                <div class="sec-2-item d-flex d-align-center d-justify-between">
+                    <div class="sec-2-icon">
+                        <img src="" alt="icon">
+                    </div>
+                    <div class="sec-2-info">
+                        <h3>Job focused Training</h3>
+                        <p>All courses cover’s Real-world Examples with the best techniques along with displaying the errors and how to resolve them.</p>
+                    </div>
+                </div>
+                <div class="sec-2-item d-flex d-align-center d-justify-between">
+                    <div class="sec-2-icon">
+                        <img src="" alt="icon">
+                    </div>
+                    <div class="sec-2-info">
+                        <h3>Job focused Training</h3>
+                        <p>All courses cover’s Real-world Examples with the best techniques along with displaying the errors and how to resolve them.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <div class="sec-3-header d-flex d-justify-between d-align-center">
+                <h2>Course List</h2>
+                <ul class="filter d-flex">
+                    <li>filter 1</li>
+                    <li>filter 2</li>
+                    <li>filter 3</li>
+                    <li>filter 4</li>
+                    <li>filter 5</li>
+                </ul>
+            </div>
+            <div class="wrapper d-flex d-wrap-row">
+                <div class="col-4 sec-3-card">
+                    <header>
+                        <img src="" alt="image">
+                    </header>
+                    <body class="sec-3-body">
+                        <h3>The Complete Blazor Bootcamp - .NET 6 (WASM and Server)</h3>
+                        <p>Build re-commerce web application using Blazor Server, Blazor WASM and .NET 6(.NET/EF Core) API and deploy to azure.</p>
+                    </body>
+                    <footer>
+                        <div class="card-btns d-flex d-justify-between d-align-center">
+                            <a href="#">buy now</a>
+                            <a href="#">details</a>
+                        </div>
+                        <div class="card-info d-flex d-justify-between d-align-center">
+                            <span>
+                                <img src="" alt="icon">
+                                <i>201 videos</i>
+                            </span>
+                            <span>
+                                <img src="" alt="icon">
+                                <i>15hr 45min</i>
+                            </span>
+                        </div>
+                    </footer>
+                </div>
+                <div class="col-4 sec-3-card">
+                    <header>
+                        <img src="" alt="image">
+                    </header>
+                    <body class="sec-3-body">
+                        <h3>The Complete Blazor Bootcamp - .NET 6 (WASM and Server)</h3>
+                        <p>Build re-commerce web application using Blazor Server, Blazor WASM and .NET 6(.NET/EF Core) API and deploy to azure.</p>
+                    </body>
+                    <footer>
+                        <div class="card-btns d-flex d-justify-between d-align-center">
+                            <a href="#">buy now</a>
+                            <a href="#">details</a>
+                        </div>
+                        <div class="card-info d-flex d-justify-between d-align-center">
+                            <span>
+                                <img src="" alt="icon">
+                                <i>201 videos</i>
+                            </span>
+                            <span>
+                                <img src="" alt="icon">
+                                <i>15hr 45min</i>
+                            </span>
+                        </div>
+                    </footer>
+                </div>
+                <div class="col-4 sec-3-card">
+                    <header>
+                        <img src="" alt="image">
+                    </header>
+                    <body class="sec-3-body">
+                        <h3>The Complete Blazor Bootcamp - .NET 6 (WASM and Server)</h3>
+                        <p>Build re-commerce web application using Blazor Server, Blazor WASM and .NET 6(.NET/EF Core) API and deploy to azure.</p>
+                    </body>
+                    <footer>
+                        <div class="card-btns d-flex d-justify-between d-align-center">
+                            <a href="#">buy now</a>
+                            <a href="#">details</a>
+                        </div>
+                        <div class="card-info d-flex d-justify-between d-align-center">
+                            <span>
+                                <img src="" alt="icon">
+                                <i>201 videos</i>
+                            </span>
+                            <span>
+                                <img src="" alt="icon">
+                                <i>15hr 45min</i>
+                            </span>
+                        </div>
+                    </footer>
+                </div>
+                <div class="col-4 sec-3-card">
+                    <header>
+                        <img src="" alt="image">
+                    </header>
+                    <body class="sec-3-body">
+                        <h3>The Complete Blazor Bootcamp - .NET 6 (WASM and Server)</h3>
+                        <p>Build re-commerce web application using Blazor Server, Blazor WASM and .NET 6(.NET/EF Core) API and deploy to azure.</p>
+                    </body>
+                    <footer>
+                        <div class="card-btns d-flex d-justify-between d-align-center">
+                            <a href="#">buy now</a>
+                            <a href="#">details</a>
+                        </div>
+                        <div class="card-info d-flex d-justify-between d-align-center">
+                            <span>
+                                <img src="" alt="icon">
+                                <i>201 videos</i>
+                            </span>
+                            <span>
+                                <img src="" alt="icon">
+                                <i>15hr 45min</i>
+                            </span>
+                        </div>
+                    </footer>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <div class="wrapper d-flex d-justify-between">
+                <div>
+                    <h2>dev courses</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint possimus aspernatur harum nihil! Dolore, odio!</p>
+                </div>
+                <div>
+                    <ul>
+                        <li><a href="#">link 1</a></li>
+                        <li><a href="#">link 2</a></li>
+                        <li><a href="#">link 3</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li><a href="#">link 1</a></li>
+                        <li><a href="#">link 2</a></li>
+                        <li><a href="#">link 3</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
