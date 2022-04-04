1ad7b83a3ef16c969f7203c26ba15224afac6109
wael alhassan <waelalhassan.fed@gmail.com>
2022-04-04 15:22:39 +0300

    m

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,27 +1,20 @@
 <footer>
     <div class="container">
-        {{-- <div class="hide-this wrapper d-grid">
+        <div class="hide-this wrapper d-grid">
             <div class="col-4 site-info">
                 <h3>About us</h3>
                 <p>{{setting('site.description')}}</p>
             </div>
             <div class="col-4 footer-nav">
+
                 <h3>footer nav 1</h3>
-                <ul>
-                    <li><a href="#">link 1</a></li>
-                    <li><a href="#">link 2</a></li>
-                    <li><a href="#">link 3</a></li>
-                </ul>
+                {{ menu('footer_center_menu')}}
             </div>
             <div class="col-4 footer-nav">
                 <h3>footer nav 2</h3>
-                <ul>
-                    <li><a href="#">link 1</a></li>
-                    <li><a href="#">link 2</a></li>
-                    <li><a href="#">link 3</a></li>
-                </ul>
+                {{ menu('footer_right_menu')}}
             </div>
-        </div> --}}
+        </div>
         <div class="hide-this copyright">
             <p>All rights reserved.© 2022. developed ❤️ by <a href="https://github.com/waelalhassan" target="_blank">Wael Alhassan</a></p>
         </div>
